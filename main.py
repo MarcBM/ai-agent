990523d72a80d41b9e311df2f92363aa9f99dd4f
MarcBM <marc.betbeder@hotmail.com>
2025-06-12 22:56:43 +1000

    1.4 - Input

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
+def main():
+  if len(sys.argv) < 2:
+    print("Usage: python main.py <prompt>")
+    sys.exit(1)
+    
+  prompt = sys.argv[1]
+  if not prompt:
+    print("Prompt cannot be empty.")
+    sys.exit(1)
+  
+  load_dotenv()
+  api_key = os.environ.get("GEMINI_API_KEY")
 
-client = genai.Client(api_key=api_key)
-
-response = client.models.generate_content(
+  client = genai.Client(api_key=api_key)
+  
+  response = client.models.generate_content(
   model="gemini-2.0-flash-001",
-  contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+  contents=prompt
   )
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+  print(response.text)
+  print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+  print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+if __name__ == "__main__":
+  main()
+  sys.exit(0)
